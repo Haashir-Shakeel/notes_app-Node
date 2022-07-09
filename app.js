0d1ff23df246ca3f03f8128e64d648f241b42e6f
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-07-09 05:54:11 +0500

    importing npm modules, chalk

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const validator = require('validator')
-
+// const validator = require('validator')
+const chalk = require('chalk')
 const getnotes = require('./notes.js')
 
 const msg = getnotes()
 console.log(msg)
 
-console.log(validator.isEmail('jhondoe@example.com'))
+const log = console.log
+log(chalk.green.bgMagenta.inverse('success') +  chalk.red.bgGreen.bold.italic.dim('Ok!') + chalk.hex('#DEADED').underline('omg'))
+// console.log(validator.isEmail('jhondoe@example.com'))
 
-console.log(validator.isURL('jhondoeexample.com'))
+// console.log(validator.isURL('jhondoeexample.com'))
