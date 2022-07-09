fee76e8db756ef4dd76eb5152047f782f6fbc87b
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-07-09 05:29:16 +0500

    importing npm modules, validator

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const validator = require('validator')
 
 const getnotes = require('./notes.js')
+
 const msg = getnotes()
 console.log(msg)
+
+console.log(validator.isEmail('jhondoe@example.com'))
+
+console.log(validator.isURL('jhondoeexample.com'))
