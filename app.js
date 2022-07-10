97000ba82270b1428651dbce4e50bc19a3620577
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-07-11 01:39:36 +0500

    options for yargs commands

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note')
+    builder: {
+        title:{
+            describe:'Note title',
+            demandOption: true, //making this option required
+            type: 'string', //to make sure we get string title instead of boolean
+        },
+        body: {
+            describe:'Note Body',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -41,5 +54,4 @@ yargs.command({
     }
 })
 
-//add, remove, read, list
-console.log(yargs.argv)
+yargs.parse() //yargs parsing
